970e89de4d61ed13131af6cce41b234357314a4d
Bart <bartpraats@hotmail.com>
2013-01-30 12:17:12 +0100

    Test klasse aangemaakt

---
+++ src/main/java/Test.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Bart
+ * Date: 30/01/13
+ * Time: 12:15
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+
+    public Test() {
+        System.out.println("Test");
+    }
+}
