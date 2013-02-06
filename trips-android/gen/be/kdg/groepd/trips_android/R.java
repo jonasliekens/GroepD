5ca2314e49852c25b2ab0e1866e5288df0e5da81
Jonas Liekens <soulscammer@gmail.com>
2013-02-06 12:09:15 +0100

    Revert "- Test Remove all"
    This reverts commit 5235137aaef440f93fe277e566143a63755f3525.

--- trips-android/gen/be/kdg/groepd/trips_android/R.java
+++
@@ -1,5 +0,0 @@
-package be.kdg.groepd.trips_android;
-
-/* This stub is for using by IDE only. It is NOT the R class actually packed into APK */
-public final class R {
-}
--- trips-web/src/main/java/Test.java
+++
@@ -1,9 +0,0 @@
-/**
- * User: Sander
- * Date: 1/02/13 9:23
- */
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("test");
-    }
-}
