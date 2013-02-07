f720b11ac6bf9882a5e1fa9424b7f5f69589cd2f
Jonas Liekens <soulscammer@gmail.com>
2013-02-07 13:07:55 +0100

    - Added a run class

---
+++ Trips/backend/src/main/java/Run.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Fluffy
+ * Date: 7/02/13
+ * Time: 12:58
+ * Copyright @ Soulware.be
+ */
+public class Run {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
