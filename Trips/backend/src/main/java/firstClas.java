124a09f4fe434a6b0197012bddb562e466897a15
Maarten Konings <konings.maarten@gmail.com>
2013-02-07 09:57:59 +0100

    Project structuur

---
+++ Trips/backend/src/main/java/firstClas.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Maarten
+ * Date: 7/02/13
+ * Time: 9:55
+ * To change this template use File | Settings | File Templates.
+ */
+public class firstClas {
+}
