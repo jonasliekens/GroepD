b97a54e32606807fd8a1ffb425378936df49f8f5
Sander Goyvaerts <sander.goyvaerts@student.kdg.be>
2013-02-19 15:46:44 +0100

    Merge branch 'master' of C:\Users\Sander\Desktop\GroepD with conflicts.

---
+++ Trips/web/src/main/java/controllers/LoginController.java
@@ -0,0 +1,15 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * User: Sander
+ * Date: 19/02/13 14:08
+ */
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+}
---
+++ Trips/web/src/main/test/controllers/LoginTest.java
@@ -0,0 +1,16 @@
+package controllers;
+
+import org.junit.Test;
+
+/**
+ * User: Sander
+ * Date: 19/02/13 14:12
+ */
+public class LoginTest {
+
+    @Test
+    public void register() {
+
+    }
+
+}
