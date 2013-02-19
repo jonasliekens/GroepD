76e9a911c501a07fc8cb2c27fd434d5637128f59
Bart Verhavert <mailme@bartverhavert.be>
2013-02-19 15:04:31 +0100

    Spring MVC fix

--- Trips/web/src/main/java/be/kdg/controllers/IndexController.java
+++ Trips/web/src/main/java/be/kdg/controllers/IndexController.java
@@ -1,22 +1,20 @@
-package controllers;
+package be.kdg.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * User: Bart Verhavert
- * Date: 11/02/13 14:36
- */
-
 @Controller
-@RequestMapping("/test")
-public class TestController {
+@RequestMapping("/")
+public class IndexController {
 
     @RequestMapping(method = RequestMethod.GET)
     public String printWelcome(ModelMap model) {
-        model.addAttribute("message", "Hello, World!");
-        return "test";
+
+        model.addAttribute("message", "Spring 3 MVC Hello World");
+        return "index";
+
     }
+
 }
