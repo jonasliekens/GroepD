088b0325fb70f655c0cf33dda04060b4383d858b
Sander Goyvaerts <sander.goyvaerts@student.kdg.be>
2013-02-24 21:12:20 +0100

    Android met Maven, aanpassingen

---
+++ Trips/android/android-it/src/main/java/be/kdg/android/test/TripsActivityTest.java
@@ -0,0 +1,14 @@
+package be.kdg.android.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * User: Sander
+ * Date: 24/02/13 - 19:58
+ */
+public class TripsActivityTest extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ Trips/android/android/src/main/java/be/kdg/android/TripsActivity.java
@@ -0,0 +1,15 @@
+package be.kdg.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * User: Sander
+ * Date: 24/02/13 - 18:41
+ */
+public class TripsActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
