05e012c86568e3e1286c0d425e55310aaad89f9b
Maarten Konings <konings.maarten@gmail.com>
2013-03-01 10:18:55 +0100

    JSON REST controller

--- Trips/backend/src/main/java/be/kdg/backend/entities/Trip.java
+++ Trips/backend/src/main/java/be/kdg/backend/entities/Trip.java
@@ -140,14 +140,6 @@ public class Trip {
         this.nrHours = nrHours;
     }
 
-    public boolean isPrivateTrip() {
-        return privateTrip;
-    }
-
-    public boolean isPublished() {
-        return published;
-    }
-
     public String getName() {
         return name;
     }
--- Trips/web/src/main/java/be/kdg/web/restcontrollers/TripRestController.java
+++ Trips/web/src/main/java/be/kdg/web/restcontrollers/TripRestController.java
@@ -1,7 +1,9 @@
 package be.kdg.web.restcontrollers;
 
 import be.kdg.backend.entities.Trip;
+import be.kdg.backend.entities.Trips;
 import be.kdg.backend.services.interfaces.TripService;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,8 +27,15 @@ public class TripRestController {
     TripService tripService;
 
     @RequestMapping(method = RequestMethod.GET)
-    public @ResponseBody List<Trip> list() {
-        //return tripService.getTrips();
-        return new ArrayList<Trip>();
+    @ResponseBody
+    public String list() {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            return objectMapper.writeValueAsString(tripService.getTrips());
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        return "";
     }
 }
