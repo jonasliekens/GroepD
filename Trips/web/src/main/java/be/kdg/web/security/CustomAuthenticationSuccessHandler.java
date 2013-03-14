e41066e07800cc1df09f104e4ad2549d0a4643fe
Bart Verhavert <mailme@bartverhavert.be>
2013-03-14 16:45:08 +0100

    Spring Security temp commit

---
+++ Trips/web/src/main/java/be/kdg/web/security/CustomAuthenticationSuccessHandler.java
@@ -0,0 +1,20 @@
+package be.kdg.web.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * User: Bart Verhavert
+ * Date: 14/03/13 13:18
+ */
+public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
